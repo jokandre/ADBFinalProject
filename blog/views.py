4ce59816525f26f9a3db7ade8f42d0cc1bf2a10b
PHE <phejimlin@gmail.com>
2016-12-18 19:39:55 +0800

    Serving static file.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from .models import User   # get_todays_recent_posts
-from flask import Flask, request, session, redirect, url_for, render_template, flash
+from flask import Flask, request, session, redirect, url_for, render_template, flash, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('test.html')
+
+# serving static file such as js css.
+@app.route('/static/<path:filename>')
+def send_static(filename):
+    return send_from_directory('static', filename)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
