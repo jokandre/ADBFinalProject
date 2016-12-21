419e485aa7fcfbb9bf1752eae894922d4d252831
PHE <phejimlin@gmail.com>
2016-12-21 14:50:32 +0800

    Finish crawl fb_api when first register.

--- blog/api/member.py
+++ blog/api/member.py
@@ -1,5 +1,6 @@
 from ..models import User   # get_todays_recent_posts
 from flask import Flask, request, session, redirect, url_for, render_template, flash, send_from_directory
+import fb_API
 
 def register():
 	json_dict = request.get_json()
@@ -7,13 +8,13 @@ def register():
 	email = json_dict['email']
 	gender = json_dict['gender']
 	Fb_id = json_dict['id']
-	print json_dict['name']
-	# username = request.form['username']
-	if not User(name, email, gender, Fb_id).register():
-		session['name'] = name
-		return redirect(url_for('main'))
-		flash('A user with that username already exists.')
-	else:
-		session['name'] = name
-		flash('Logged in.')
-		return redirect(url_for('main'))
+	access_token = json_dict['access_token']
+
+	if User(name, email, gender, Fb_id).register():
+		#  Not yet register before.
+		fb_API.get_fb_frends(Fb_id, access_token)
+		fb_API.get_fb_likes(Fb_id, access_token)
+
+	session['Fb_id'] = Fb_id
+	return redirect(url_for('main'))
+	flash('Logged in.')
