faba3a165acafdaa16a17ff21cbc46c458be2897
eunicebes <eunicebes@gmail.com>
2017-01-04 15:35:54 +0800

    Update changes

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from .api import member
 from .api import diary
+# from .api import pair
 from flask import Flask, request, session, redirect, url_for, render_template, flash, send_from_directory, jsonify
 from invalidusage import InvalidUsage
 from flask.json import jsonify
@@ -13,7 +14,18 @@ def index():
 
 @app.route('/index', methods=['GET'])
 def main():
+    session_check('render_page')
     return render_template('index.html')
+
+@app.route('/create_diary', methods=['GET'])
+def create_diary():
+    session_check('render_page')
+    return render_template('create_diary.html')
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    session_check('render_page')
+    return render_template('profile.html')
 
 # serving static file such as js css.
 @app.route('/static/<path:filename>')
@@ -26,10 +38,17 @@ def login():
         return member.register()
     return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    # remove the id from the session if it's there
+    session.pop('id', None)
+    return redirect(url_for('login'))
+
 @app.route('/diary/<path:path>', methods=['GET', 'POST'])
 def diary_API(path):
-    print 'You want path: %s' % path
+    print 'Request path: %s' % path
     if request.method == 'GET':
+        # session_check('api')  # when online uncomment this.
         # API GET: /diary/api/v1/get?id=x
         if path == 'api/v1/get':
            return diary.get_all_diary()
@@ -44,8 +63,39 @@ def diary_API(path):
     else:
         raise InvalidUsage("Something Wrong.", 404)
 
+# @app.route('/pair/<path:path>', methods=['GET', 'POST'])
+# def pair_API(path):
+#     print 'Request path: %s' % path
+#     if request.method == 'GET':
+#         # session_check('api')  # when online uncomment this.
+#         # API GET: /pair/api/v1/get?id=x
+#         if path == 'api/v1/get':
+#            return pair.get_all_diary()
+#         else:
+#             raise InvalidUsage("Wrong URL", 404)
+#     elif request.method == 'POST':
+#         # API POST: /pair/api/v1/create
+#         if path == 'api/v1/create':
+#             return pair.create()
+#         else:
+#             raise InvalidUsage("Wrong URL", 404)
+#     else:
+#         raise InvalidUsage("Something Wrong.", 404)
+
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
+    if error.get_error_type() == 'redirectToLoginPage':
+        return redirect(url_for('login'))
+    else:
+        response = jsonify(error.to_dict())
+        response.status_code = error.status_code
+        return response
+
+def session_check(request_type):
+    # request_type only will be api or render_page.
+    if request_type == 'api':
+        if 'id' not in session:
+            raise InvalidUsage("unauthorized", 401)
+    elif request_type == 'render_page':
+        if 'id' not in session:
+            raise InvalidUsage(None, None, None, "redirectToLoginPage")
