d1906f97b64ebf7b6d519d345a9b4bf993e28707
MoriNakamura <f0923206757@yahoo.com.tw>
2017-01-04 17:53:53 +0800

    doing nearby member

--- blog/models.py
+++ blog/models.py
@@ -122,7 +122,7 @@ class User:
         return graph.run(query, they=other.username, you=self.username).next
 
     @staticmethod
-    def update_location(node_id, lat, lon):
+    def update_location(uid, lat, lon):
         query = '''
         MATCH (u) WHERE u.id = {which}
         SET u.wkt = {wkt}
@@ -131,7 +131,11 @@ class User:
         RETURN COUNT(node)
         '''
 
-        return graph.run(query, which=node_id, wkt=lon_lat_to_wkt(lon, lat))
+        return graph.run(query, which=uid, wkt=lon_lat_to_wkt(lon, lat))
+
+    # @staticmethod
+    # def get_nearby_member(uid, distance):
+
 
 
 def get_todays_recent_posts():
